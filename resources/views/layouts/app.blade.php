0ba5dc26c2e033f72a0c418b253c0417973c0116
mohamed <mo.b3n.saber@gmial.com>
2025-10-03 10:20:49 +0300

    add readme file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,11 +66,7 @@
         </div>
 
 
-        <!-- News Content Start -->
-        <section class="sales text-center p-2 d-block d-lg-none">
-            شحن مجاني للطلبات 💥 عند الشراء ب 699ج او اكثر
-        </section>
-        <!-- News Content End -->
+
         @yield('content')
     </div>
     @include('inc.footer')
